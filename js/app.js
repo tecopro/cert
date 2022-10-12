31c554f7a8a549e66f6e08d8f5e1205b3702bac7
sofa-emha <mail@sofa.my.id>
2022-10-12 19:38:53 +0700

    :black_nib: refactoring

--- js/app.js
+++ js/app.js
@@ -1,3 +1,32 @@
+/**
+ * @description toggle settings menu
+ */
+try {
+    function toggleSwitcher() {
+        var i = document.getElementById('style-switcher');
+        if (i.style.left === "-189px") {
+            i.style.left = "0px";
+        } else {
+            i.style.left = "-189px";
+        }
+    };
+    
+    function setColor(theme) {
+        document.getElementById('color-opt').href = './css/colors/' + theme + '.css';
+        toggleSwitcher(false);
+    };
+    
+    function setTheme(theme) {
+        document.getElementById('theme-opt').href = './css/' + theme + '.min.css';
+        toggleSwitcher(false);
+    };
+} catch (error) {
+    new Error(error)
+}
+
+/**
+ * @description remove preloader if window on load
+ */
 window.onload = function loader() {
     if(document.getElementById('preloader')){
         document.getElementById('preloader').style.visibility = 'hidden';
@@ -5,4 +34,7 @@ window.onload = function loader() {
     }
 }
 
+/**
+ * @description replace feather
+ */
 feather.replace();
